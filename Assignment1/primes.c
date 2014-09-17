c19245631d00af9a11308aedd87fa875325d0512
Suketu Shah <stevenssshah75@gmail.com>
2014-09-16 23:18:17 -0400

    Documentation

--- Assignment1/primes.c
+++ Assignment1/primes.c
@@ -4,7 +4,10 @@
 *	Practice with UNIX Process Mechanisms
 */	
 
-/* Project Description: The purpose of this assignment is to acquire some experience with UNIX mechanisms that are helpful in writing multi-process programs. Program called "primes" accepts a sequence of increasing positive numbers on the command line. The integers define range and a child process is created to search for primes in each range. */
+/* 	Project Description: The purpose of this assignment is to acquire experience with UNIX mechanisms that are helpful in writing multi-process programs. 
+*	Program called "primes" accepts a sequence of increasing positive numbers on the command line. 
+*	The integers define range and a child process is created to search for primes in each range. 
+*/
 
 /* All the header files required for this assignment */
 

9ef85effb89f00479eed721ce2112348ce8f3a3c
sshah93 <stevenssshah75@gmail.com>
2014-10-10 20:08:29 -0400

    monitor

--- Assignment3/monitor.h
+++ Assignment3/monitor.h
@@ -1,4 +1,12 @@
 #ifndef _MONITOR_
 #define _MONITOR_
 
+#include "q.h"
+
+extern void monitor_init();
+extern void monitor_arrive();
+extern void monitor_cross();
+extern void monitor_leave();
+extern void monitor_shutdown();
+
 #endif

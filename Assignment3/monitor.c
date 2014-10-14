7d6f27143dc0f8924b0f6024f80048ff850eb841
sshah93 <stevenssshah75@gmail.com>
2014-10-14 15:25:07 -0400

    small changes

--- Assignment3/monitor.c
+++ Assignment3/monitor.c
@@ -1,5 +1,30 @@
 #include "monitor.h"
 
+void monitor_init()
+{
+	
+}
+
+void monitor_arrive()
+{
+
+}
+
+void monitor_cross()
+{
+
+}
+
+void monitor_leave()
+{
+
+}
+
+void monitor_shutdown()
+{
+
+}
+
 int main()
 {
 	return 0;

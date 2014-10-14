011411040f0cd5b7777b2fd3daeda2068643e5bb
sshah93 <stevenssshah75@gmail.com>
2014-10-14 17:46:39 -0400

    Monitor.c function implementation

--- Assignment3/monitor.c
+++ Assignment3/monitor.c
@@ -2,30 +2,208 @@
 
 void monitor_init()
 {
-	
+	if(pthread_mutex_init(&lock, NULL) != 0)
+	{
+		fprintf(stderr, "Failed to initialize the mutex!\n");
+		exit(1);
+	}
+
+	if(pthread_cond_init(&north, NULL) != 0)
+	{
+		fprintf(stderr, "Failed to initialize the north conditional variable!\n");
+		exit(1);
+	}
+
+	if(pthread_cond_init(&south, NULL) != 0)
+	{
+		fprintf(stderr, "Failed to initialize the south conditional variable!\n");
+		exit(1);
+	}
+
+	if(pthread_cond_init(&east, NULL) != 0)
+	{
+		fprintf(stderr, "Failed to initialize the east conditional variable!\n");
+		exit(1);
+	}
+
+	if(pthread_cond_init(&west, NULL) != 0)
+	{
+		fprintf(stderr, "Failed to initialize the west conditional variable!\n");
+		exit(1);
+	}
+
+	directions[0] = Q_NORTH;
+	directions[1] = Q_WEST;
+	directions[2] = Q_SOUTH;
+	directions[3] = Q_EAST;
+
+	if(q_cartIsWaiting(Q_NORTH))
+	{
+		direction = 0;
+	}
+	else if(q_cartIsWaiting(Q_WEST))
+	{
+		direction = 1;
+	}
+	else if(q_cartIsWaiting(Q_SOUTH))
+	{
+		direction = 2;
+	}
+	else if(q_cartIsWaiting(Q_EAST))
+	{
+		direction = 3;
+	}
 }
 
-void monitor_arrive()
+void monitor_arrive(cart_t* cart)
 {
+	if(pthread_mutex_lock(&lock) != 0)
+	{
+		fprintf(stderr, "Couldn't get the lock!\n");
+		exit(1);
+	}
 
+	printf("Cart %s arrived at intersection!\n", cart->num);
+
+	while(directions[direction] != cart->dir)
+	{
+		printf("Cart has to wait before it can enter the intersection!\n");
+
+		switch(cart->dir)
+		{
+			case Q_NORTH:
+				if(pthread_cond_wait(&north, &lock) != 0)
+				{
+					fprintf(stderr, "Wait function messed up!\n");
+					exit(1);
+				}
+
+			case Q_SOUTH:
+				if(pthread_cond_wait(&south, &lock) != 0)
+				{
+					fprintf(stderr, "Wait function messed up!\n");
+					exit(1);
+				}
+
+			case Q_EAST:
+				if(pthread_cond_wait(&east, &lock) != 0)
+				{
+					fprintf(stderr, "Wait function messed up!\n");
+					exit(1);
+				}
+
+			case Q_WEST:
+				if(pthread_cond_wait(&west, &lock) != 0)
+				{
+					fprintf(stderr, "Wait function messed up!\n");
+					exit(1);
+				}
+
+			default:
+				printf("Something is not correct here!\n");
+		}
+
+		printf("Cart %s allowed to proceed into intersection!\n", cart->num);
+	}
 }
 
-void monitor_cross()
+void monitor_cross(cart_t* cart)
 {
+	q_cartHasEntered(directions[direction]);
 
+	printf("Cart %s entered the intersection!\n", cart->num);
+
+	usleep(10000000);
+
+	printf("Cart %s crosses the intersection!\n", cart->num);
 }
 
-void monitor_leave()
+void monitor_leave(cart_t* cart)
 {
+	empty = 0;
 
+	printf("Cart %s leaves the intersection!\n", cart->num);
+
+	unsigned int i;
+
+	/* Check directions to the right for waiting carts */
+	for (i = 0; i < 4; i++)
+	{
+		if(q_cartIsWaiting(directions[(direction + i) % 4]))
+		{
+			direction = (direction + i) % 4;
+
+			switch(directions[direction])
+			{
+				case Q_NORTH:
+					if(pthread_cond_signal(&north) != 0)
+					{
+						printf("Thread %c signals thread %c", cart->dir, Q_NORTH)
+						fprintf(stderr, "Failed to signal!\n");
+						exit(1);
+					}
+				case Q_WEST:
+					if(pthread_cond_signal(&west) != 0)
+					{
+						printf("Thread %c signals thread %c", cart->dir, Q_WEST)
+						fprintf(stderr, "Failed to signal!\n");
+						exit(1);
+					}
+				case Q_SOUTH:
+					if(pthread_cond_signal(&south) != 0)
+					{
+						printf("Thread %c signals thread %c", cart->dir, Q_SOUTH)
+						fprintf(stderr, "Failed to signal!\n");
+						exit(1);
+					}
+				case Q_EAST:
+					if(pthread_cond_signal(&east) != 0)
+					{
+						printf("Thread %c signals thread %c", cart->dir, Q_EAST)
+						fprintf(stderr, "Failed to signal!\n");
+						exit(1);
+					}
+			}
+			break;
+		}
+	}
+
+	if(pthread_mutex_unlock(&lock) != 0)
+	{
+		fprintf(stderr, "Failed to unlock the mutex!\n");
+		exit(1);
+	}
 }
 
 void monitor_shutdown()
 {
+	if(pthread_mutex_destroy(&lock) != 0)
+	{
+		fprintf(stderr, "Failed to destroy the mutex!\n");
+		exit(1);
+	}
 
-}
+	if(pthread_cond_destroy(north) != 0)
+	{
+		fprintf(stderr, "Failed to destroy the conditional variable!\n", );
+		exit(1);
+	}
 
-int main()
-{
-	return 0;
+	if(pthread_cond_destroy(west) != 0)
+	{
+		fprintf(stderr, "Failed to destroy the conditional variable!\n", );
+		exit(1);
+	}
+
+	if(pthread_cond_destroy(south) != 0)
+	{
+		fprintf(stderr, "Failed to destroy the conditional variable!\n", );
+		exit(1);
+	}
+
+	if(pthread_cond_destroy(east) != 0)
+	{
+		fprintf(stderr, "Failed to destroy the conditional variable!\n", );
+		exit(1);
+	}
 }
--- Assignment3/monitor.h
+++ Assignment3/monitor.h
@@ -2,11 +2,20 @@
 #define _MONITOR_
 
 #include "q.h"
+#include "pthread.h"
+#include "stdio.h"
+
+pthread_mutex_t lock;
+pthread_cond_t turn;
+
+int empty;
+
+char direction;
 
 extern void monitor_init();
-extern void monitor_arrive();
-extern void monitor_cross();
-extern void monitor_leave();
+extern void monitor_arrive(cart_t*);
+extern void monitor_cross(cart_t*);
+extern void monitor_leave(cart_t*);
 extern void monitor_shutdown();
 
 #endif

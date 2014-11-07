8a743e17256c567e4759e2e79c80781f1f48528a
Suketu Shah <stevenssshah75@gmail.com>
2014-11-07 12:27:50 -0500

    fixing arith function

--- Assignment5/lab.erl
+++ Assignment5/lab.erl
@@ -5,9 +5,9 @@
 
 arith(X, Y) -> io:fwrite("Arguments: ~p ~p~n", [X, Y]),
 	Sum = X + Y,
-	io:fwrite("~p~n", [Sum]) {Sum}.
+	io:fwrite("newThread: Sum = ~p", [Sum]).
 
 start() ->
-	{ok, [X1, X2]} = io:fread("first_thread: enter two integers please> ", "~d~d"),
+	{ok, [X1, X2]} = io:fread("first_thread: enter two integers please> ", "~p~p"),
 	spawn(newThread, arith, [X1, X2]),
 	halt().

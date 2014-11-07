0225c3c67cc0ee7e85fc1b6abd8e478651b78b9c
Suketu Shah <stevenssshah75@gmail.com>
2014-11-07 12:25:03 -0500

    2 functions

--- Assignment5/lab.erl
+++ Assignment5/lab.erl
@@ -1,1 +1,13 @@
-%"""CS511 Erlang Lab"""
+%%CS511 Erlang Lab
+
+-module(lab).
+-export([start/0, arith/2]).
+
+arith(X, Y) -> io:fwrite("Arguments: ~p ~p~n", [X, Y]),
+	Sum = X + Y,
+	io:fwrite("~p~n", [Sum]) {Sum}.
+
+start() ->
+	{ok, [X1, X2]} = io:fread("first_thread: enter two integers please> ", "~d~d"),
+	spawn(newThread, arith, [X1, X2]),
+	halt().

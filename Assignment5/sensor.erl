d972934ff484fb81213e119bd2b0471472697e1e
mpeleshenko <mpeleshe@stevens.edu>
2014-11-10 17:16:27 -0500

    Initial sensor

--- Assignment5/sensor.erl
+++ Assignment5/sensor.erl
@@ -2,3 +2,18 @@
 %% Assignment 5
 %% Suketu Shah and Michael Peleshenko
 %% sensor.erl
+-module(sensor).
+-export([ sensor_run()/2 ]).
+
+sensor_run(Watcher_pid, Sensor_id) ->
+	random:seed(now()) ,
+	Sleep_time = random:uniform(10000) ,
+	timer:sleep(Sleep_time) ,
+	Measurement = random:uniform(11) ,
+	case Measurement of
+		11 ->
+			exit(Watcher_pid, 'anomalous_reading') ;
+		_ ->
+			Watcher_pid ! {Sensor_id, Measurement}
+	end ,
+	sensor_measure(Watcher_pid, Sensor_id) .

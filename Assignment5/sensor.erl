47364eb90203b25f50082291b6b672d853c48bcc
mpeleshenko <mpeleshe@stevens.edu>
2014-11-12 17:20:03 -0500

    working version

--- Assignment5/sensor.erl
+++ Assignment5/sensor.erl
@@ -5,15 +5,29 @@
 -module(sensor).
 -export([ sensor_run/2 ]).
 
+%% Function to start sensor
+%% Watcher_pid - PID of watcher
+%% Sensor_id - ID of sensor
 sensor_run(Watcher_pid, Sensor_id) ->
+	%% Set random seed
 	random:seed(now()) ,
+	%% Start measuring
+	sensor_measure(Watcher_pid, Sensor_id) .
+
+%% Function to send measurements to watcher
+%% Watcher_pid - PID of watcher
+%% Sensor_id - ID of sensor
+sensor_measure(Watcher_pid, Sensor_id) ->
+	%% Get random sleep time
 	Sleep_time = random:uniform(10000) ,
+	%% Sleep
 	timer:sleep(Sleep_time) ,
+	%% Generate random measurement
 	Measurement = random:uniform(11) ,
-	case Measurement of
-		11 ->
+	if
+		Measurement == 11 -> %% Crash if measurement reading is 11
 			exit('anomalous_reading') ;
-		_ ->
+		true -> %% Otherwise, send measurement to watcher
 			Watcher_pid ! {Sensor_id, Measurement}
 	end ,
 	sensor_run(Watcher_pid, Sensor_id) .
--- Assignment5/watcher.erl
+++ Assignment5/watcher.erl
@@ -6,11 +6,14 @@
 -export([ watcher_start/0, watcher_run/2, watcher_spawn/2, watcher_receive/1 ]) .
 
 watcher_start() ->
+	%% Get number of sensors to create from user
 	{ok, [ Sensors ]} = io:fread("enter number of sensors to start> ", "~d") ,
+	%% Spawn watcher threads
 	watcher_spawn(Sensors, 0) .
 
 %% Function to spawn Sensors sensors
-%% the Watcher_group represents that starting sensor ID for a given watcher
+%% Sensors - number of sensors to spawn
+%% Watcher_group - represents the ten's place of the sensor ID for a given watcher
 %% i.e. 0 -> 0, 1 -> 10, 2 -> 20, ..., 10 -> 100
 watcher_spawn(Sensors, Watcher_group) ->
 	if
@@ -20,48 +23,62 @@ watcher_spawn(Sensors, Watcher_group) ->
 			_Pid = spawn(?MODULE, watcher_run, [ Watcher_group, 10 ]) ,
 			watcher_spawn(Sensors - 10, Watcher_group + 1) ;
 		true ->
-			timer:sleep(10000)
+			exit('done spawning')
 	end .
 
+%% Function to start sensons and receiving messages
+%% Watcher_group - represents the ten's place of the sensor ID for a given watcher
+%% Count - number of sensors to spawn
 watcher_run(Watcher_group, Count) ->
-	%% Spawn Count sensors
+	%% Spawn sensors for given watcher thread
 	Sensors = sensor_spawn(Watcher_group, Count, 0, []) ,
-	io:fwrite("STARTED: Watcher started with sensors: ~n~p~n", [Sensors]) ,
+	%%io:fwrite("STARTED: Watcher started with sensors: ~n~p~n", [ Sensors ]) ,
+	%% start processing received messages
 	watcher_receive(Sensors) .
 
+%% Function to spawn sensors
+%% Watcher_group - represents the ten's place of the sensor ID for a given watcher
+%% Count - number of sensors to spawn
+%% Start_id - number [0-9] that represents one's place in sensor ID
+%% Sensors_list - list of {Sensor ID, PID} for sensors
 sensor_spawn(Watcher_group, Count, Start_id, Sensors_list) ->
 	if
-		Count == 0 ->
+		Count == 0 -> %% Return sensor list when done spawning sensors
 			Sensors_list ;
-		Count > 0 ->
+		Count > 0 -> %% Spawn sensor
+			%% Calculate sensor ID
 			Sensor_id = Watcher_group * 10 + Start_id ,
+			%% Spawn sensor with a monitor
 			{ Pid, _ } = spawn_monitor(sensor, sensor_run, [self(), Sensor_id]) ,
-			%% Append {Sensor_id, PID} tuple to list of sensors
-			New_sensors_list = lists:keystore(Pid, 2, Sensors_list, {Sensor_id, Pid}) ,
+			%% Add {Sensor ID, PID} tuple to list of sensors
+			New_sensors_list = lists:keystore(Sensor_id, 1, Sensors_list, {Sensor_id, Pid}) ,
 			sensor_spawn(Watcher_group, Count - 1, Start_id + 1, New_sensors_list) ;
 		true ->
-			io:fwrite("Invalid count~n", [])
+			io:fwrite("Invalid count~n")
 	end .
-	
+
+%% Function to receive measurements from sensors
+%% Sensors - list of sensors
 watcher_receive(Sensors) ->
 	receive
-		{Sensor_id, Measurement} ->
-			io:fwrite("RECEIVED: measurement ~p from sensor ~p~n", [Measurement, Sensor_id]) ;
-		{'DOWN', _Ref, process, Pid, Reason} ->
+		{Sensor_id, Measurement} -> %% Handle proper measurement from sensor
+			io:fwrite("RECEIVED: measurement ~p from sensor ~p~n", [Measurement, Sensor_id]) ,
+			watcher_receive(Sensors) ;
+		{'DOWN', _Ref, process, Pid, Reason} -> %% Handle anomalous reading from sensor
 			%% Get Sensor_id from list
-			Sensor_id = lists:keyfind(Pid, 2, Sensors) ,
+			{Sensor_id, _Pid} = lists:keyfind(Pid, 2, Sensors) ,
 			if
 				Sensor_id == false ->
 					io:fwrite("Could not find sensor PID ~p in sensor list~n", [ Pid ]) ;
 				true ->
 					io:fwrite("DIED: Sensor ~p died with reason ~p~n", [ Sensor_id, Reason ]) ,
-					%% Restart sensor
-					{ Pid, _ } = spawn_monitor(sensor, sensor_run, [self(), Sensor_id]) ,
-					New_sensors = lists:keyreplace(Pid, 2, Sensors, {Sensor_id, Pid}) ,
-					io:fwrite("RESTARTED: A sensor was restarted, updated list of sensors: ~n~p~n", [Sensors]) ,
+					%% Spawn new sensor
+					{ New_pid, _ } = spawn_monitor(sensor, sensor_run, [ self(), Sensor_id ]) ,
+					%% Update list of sensors with new PID
+					New_sensors = lists:keyreplace(Sensor_id, 1, Sensors, {Sensor_id, New_pid}) ,
+					io:fwrite("RESTARTED: A sensor was restarted, updated list of sensors: ~n~p~n", [ New_sensors ]) ,
 					watcher_receive(New_sensors)
 			end ;
 		_ ->
 			io:fwrite("Received unknown message~n")
 	end .
-			
